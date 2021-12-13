ac75f6ec8e95f76241a2f7a1ce011843262f2567
brianorora <brianorora@gmail.com>
2021-12-13 17:57:20 +0300

    Add `Student` class

---
+++ teacher.rb
@@ -0,0 +1,13 @@
+require_relative('./person')
+
+class Teacher < Person
+  def initialize(*args, **kwargs)
+    super(*args, **kwargs)
+    @specialization = nil
+  end
+
+  def can_use_services?
+    true
+  end
+
+end
