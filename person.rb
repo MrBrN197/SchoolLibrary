761915d2d890a172fe17effe5ad62d8f02c0caf1
brianorora <brianorora@gmail.com>
2021-12-13 17:37:20 +0300

    Add  class `Person`

---
+++ person.rb
@@ -0,0 +1,22 @@
+class Person
+  attr_accessor :name, :age
+  attr_reader :id
+
+  def initialize(age, name: 'Unknown', parent_permission: true)
+    @id = 234_234
+    @name = name
+    @age = age
+    puts name
+    @parent_permission = parent_permission
+  end
+
+  def can_use_services
+    is_of_age? || @parent_permission
+  end
+
+  private
+
+  def is_of_age?
+    @age > 18
+  end
+end
