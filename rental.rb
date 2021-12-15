d9f545851c7cf4709330d10c85705e04834f9d68
brianorora <brianorora@gmail.com>
2021-12-15 14:37:33 +0300

    Create rental `belongs-to` owner relation

--- rental.rb
+++ rental.rb
@@ -11,5 +11,10 @@ class Rental
     @book = book
     @book.rentals.push(self) unless @book.rentals.include?(self)
   end
+  
+  def owner=(owner)
+    @owner = owner
+    @owner.rentals.push(self) unless @owner.rentals.include?(self)
+  end
 
 end
