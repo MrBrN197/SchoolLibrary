1bade14931f7c8803addb803add05777aed0d93e
brianorora <brianorora@gmail.com>
2021-12-15 14:03:46 +0300

    Add `classroom` setter

--- student.rb
+++ student.rb
@@ -3,7 +3,12 @@ require_relative('./person')
 class Student < Person
   def initialize(*args, classroom:, **kwargs)
     super(*args, **kwargs)
+    self.classroom = classroom
+  end
+  
+  def classroom=(classroom)
     @classroom = classroom
+    @classroom.students.push(self) unless @classroom.students.include?(self)
   end
 
   def play_hooky
