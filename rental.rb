81d51c85951cb19df1aa271ac42bf81ba7f16241
brianorora <brianorora@gmail.com>
2021-12-15 14:56:48 +0300

    Set book and person in `Rental` constructor

--- rental.rb
+++ rental.rb
@@ -3,16 +3,20 @@ require 'date'
 class Rental
   attr_accessor :date
 
-  def initialize(date = DateTime.now())
+  def initialize(book , owner, date = DateTime.now())
     @date = date
+    self.book = book
+    self.owner = owner
   end
 
   def book=(book)
+    puts "Setting Book"
     @book = book
     @book.rentals.push(self) unless @book.rentals.include?(self)
   end
   
   def owner=(owner)
+    puts "Setting Owner"
     @owner = owner
     @owner.rentals.push(self) unless @owner.rentals.include?(self)
   end
