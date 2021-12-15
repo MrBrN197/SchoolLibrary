4eda2fb103cbde697b37157b22f45b7eb900d699
brianorora <brianorora@gmail.com>
2021-12-15 14:24:16 +0300

    Create `has-many` rentals relationship for book

--- book.rb
+++ book.rb
@@ -1,9 +1,16 @@
 class Book
 
-  attr_accessor :title, :author
+  attr_accessor :title, :author, :rentals
 
   def initialize(title, author)
     @title = title
     @author = author
+    @rentals = []
   end
+
+  def add_rental(rental)
+    @rentals.push(rental) unless @rentals.include?(rental)
+    rental.book = self
+  end
+
 end
